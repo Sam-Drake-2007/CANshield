e1257d21afc1d82824706d915ca470524cec54b8
Samuel Drake <samueljhdrake@gmail.com>
2026-01-17 13:42:40 -0500

    downloaded graphing package

--- src/pages/results/ResultsPage.jsx
+++ src/pages/results/ResultsPage.jsx
@@ -2,8 +2,6 @@ import bgImage from '../../assets/images/main-bg.png';
 
 export default function Results() {
 
-    const bg_url = "https://images.unsplash.com/photo-1651771529172-0d45a1455598?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-
     const coverage_stats = [
         { id: 1, name: 'Average Coverage', value: '73%' },
         { id: 2, name: 'Min Coverage', value: '30%' },
