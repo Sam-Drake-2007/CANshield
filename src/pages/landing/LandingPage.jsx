abddd592c05001931b0c7cbdd3438d579e91034e
ThomasPingot <thom.pingot@gmail.com>
2026-01-17 10:12:22 -0500

    Added Assets Folder

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
             {/* WRITE THE CSS DIRECTLY IN the 'className' attribute, look up the equivalent CSS style on TailwindCSS' site if needed */}
             <h1 className="text-red-500 font-bold text-2xl">This is the landing page.</h1>
             <p className="mt-5 text-blue-400 text-lg">This is another piece of text.</p>
-            <p>The only 2 other routes </p>
+            <p>The only 2 other routes blabla </p>
         </main>
     );
 }
